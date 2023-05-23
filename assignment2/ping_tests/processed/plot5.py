42da2b024aa41f46ff7eccd92084942e379d30a8
Lee Chih Jung <54738966+LordJatonyas@users.noreply.github.com>
2023-05-23 01:02:29 +0100

    CDF Plotter refined

---
+++ assignment2/ping_tests/processed/plot5.py
@@ -0,0 +1,29 @@
+# !/usr/bin/python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 [filename]")
+        sys.exit()
+    filename = sys.argv[1]
+    number = filename[-5]
+    xlabel = "time"
+    ylabel = "probability"
+    title = f'CDF for Interval={10**(-number/10)}'
+    fig_name = f'ping5-{number}.png'
+
+    t = np.loadtxt(filename, delimiter=" ", dtype="float")
+    x = np.sort(t)
+    y = 1.00 * np.arange(len(t)) / (len(t) - 1)
+
+    plt.plot(x,y)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.savefig(fig_name)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
